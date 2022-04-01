a4e72560cfb16a623069f3b89b0703725d9caf2e
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-01 15:04:10 +0530

    sum of nos upto n

--- day1.cpp
+++ day1.cpp
@@ -34,3 +34,18 @@ int main(){
         cout<<"Odd"<<endl;
     }
     return 0;
+}
+//WAP to do sum of numbers upto n
+# include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    int sum=0;
+    for(int counter=1;counter<=n;counter++){
+        sum=sum+counter;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
