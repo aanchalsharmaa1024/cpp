bd0f0d621a12795789254ad58f4d9b4a085f0cbb
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-01 23:35:39 +0530

    for loop

---
+++ day2.cpp
@@ -0,0 +1,14 @@
+//WAP to do sum of numbers upto n
+# include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    int sum=0;
+    for(int counter=1;counter<=n;counter++){
+        sum=sum+counter;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
