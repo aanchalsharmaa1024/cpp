88d8ed6f39ac66171433a9db210b7cd40ea637e2
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-02 18:56:42 +0530

    no. is prime or not

--- day1.cpp
+++ day1.cpp
@@ -144,3 +144,22 @@ int main(){
     }
     return 0;
 }
+# include <iostream>
+using nmaespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i;
+    
+    for(i=2;i<n;i++){
+        if(n%i==0){
+            cout<<"non-prime"<<endl;
+            break;
+        }
+
+    }
+    if(i==n){
+        cout<<"Prime"<<endl;
+    }
+    return 0;
+}
