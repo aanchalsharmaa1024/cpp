e19c0bcd885faf5ee2bceb1e8508c0aea3bbcb1a
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-02 18:13:39 +0530

    jumps in loops

--- day1.cpp
+++ day1.cpp
@@ -119,11 +119,16 @@ int main(){
 #include<iostream>
 using nmaespace std;
 int main(){
+    int pocketMoney=3000;
     for(int data=1; data<=30; date++){
         if(data%2==0){
             continue;
         }
+        if(pocketMoney=0){
+            break;
+        }
         cout<<"Go out Toady!"<<endl;
+        pocketMoney=pocketMoney-300;
     }
 return 0;
 }
---
+++ day3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using nmaespace std;
+int main(){
+    int pocketMoney=3000;
+    for(int data=1; data<=30; date++){
+        if(data%2==0){
+            continue;
+        }
+        if(pocketMoney=0){
+            break;
+        }
+        cout<<"Go out Toady!"<<endl;
+        pocketMoney=pocketMoney-300;
+    }
+return 0;
+}
+
+
