c88ed98421b3fad27708677811cb2d9a21ffbfd1
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-02 23:04:39 +0530

    new code

--- day1.cpp
+++ day1.cpp
@@ -244,3 +244,23 @@ int main(){
     }
     return 0;
 }
+
+# include <iostream>
+using namespace std;
+int main(){
+    int i=1;
+    i=i++ + ++i;
+    cout<<i<<endl;
+    return 0;
+}
+
+# include <iostream>
+using nmaespace std;
+int main(){
+    int i=1;
+    int j=2;
+    int k;
+    k = i + j + i++ + j++  + ++i + ++j;
+    cout<<i<<" "<<j<<" "<<" "<<k<<endl;
+    return 0 ;
+}
--- day5.cpp
+++ day5.cpp
@@ -60,3 +60,11 @@ int main(){
     }
     return 0;
 }
+# include <iostream>
+using namespace std;
+int main(){
+    int i=1;
+    i=i++ + ++i;
+    cout<<i<<endl;
+    return 0;
+}
