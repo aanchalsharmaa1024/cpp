8ddf021dbe6841cf37a070296fab6088342747c0
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-02 19:27:37 +0530

    prime nos.

--- day1.cpp
+++ day1.cpp
@@ -163,3 +163,23 @@ int main(){
     }
     return 0;
 }
+# include <iostream>
+using namespace std
+int main(){
+    int a,b;
+    cin>>a>>b;
+    for(num=a; num<=b; num++){
+        int i;
+        for(i=2; i<num; i++){
+            if(num%i==0){
+                break;
+            }
+        }
+            if(i==num){
+                cout<<num<<endl;
+            }
+        
+    }
+    return 0;
+
+}
