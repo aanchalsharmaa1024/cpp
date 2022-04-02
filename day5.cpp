9148b52f71e45fcbaaaebb62a269c30812d38bc6
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-02 19:43:30 +0530

    switch case

--- day5.cpp
+++ day5.cpp
@@ -1,2 +1,30 @@
 //switch-case statement
+# include <iostream>
+using namespace std
+int main(){
+    char button;
+    cout<<"input a character:";
+    cin>>button;
+    switch(button){
+        case'a':
+        cout<<"hello"<<endl;
+        break;
+        case'b':
+        cout<<"namaste"<<endl;
+        break;
+        case'c':
+        cout<<"salute"<<endl;
+        break;
+        case'd':
+        cout<<"hola"<<endl;
+        break;
+        case'e':
+        cout<<"ciao"<<endl;
+        break;
+        default:
+        cout<<"I am still learning more"<<endl;
+        break;
 
+    }
+    return 0;
+}
