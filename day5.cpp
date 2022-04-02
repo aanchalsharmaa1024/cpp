857b011f461e67f6643a808374c71c7ed06c7e90
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-02 22:18:03 +0530

    simple calculator

--- day5.cpp
+++ day5.cpp
@@ -29,3 +29,34 @@ int main(){
     return 0;
 }
 //simple calculator
+# include <iostream>
+using namespace std;
+int main(){
+    float n1,n2;
+    cout<<"input 2 nos.:";
+    cin>>n1>n2;
+
+    char op;
+    cout<<"input 2 operators";
+    cin>>op;
+
+    switch(op)
+    {
+        case:'+'
+        cout<<n1+n2<<endl;
+        break;
+         case:'-'
+        cout<<n1-n2<<endl;
+        break;
+         case:'/'
+        cout<<n1/n2<<endl;
+        break;
+         case:'*'
+        cout<<n1*n2<<endl;
+        break;
+        default:
+        cout<<"enter another operator"<<endl;
+
+    }
+    return 0;
+}
