fd8b8650798fc6a3acc6f8aadff9ee07560fc9e3
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-03 13:50:32 +0530

    code added

--- day5.cpp
+++ day5.cpp
@@ -114,4 +114,21 @@ int main(){
     return 0;
     }
 //WAP t0 print that input no. is equal to , less than or more than 10
+# include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
 
+    if(n>10){
+        cout<<"more than 10"<<endl;
+
+    }
+    else if(n<10){
+        cout<<"less than 10"<<endl;
+    }
+    else {
+        cout<<"equal to 10"<<endl;
+    }
+    return 0;
+}
