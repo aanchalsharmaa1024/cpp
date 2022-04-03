cc507ee628a69169580606f8af41c34bac78e6d0
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-03 16:09:30 +0530

    code added

--- day5.cpp
+++ day5.cpp
@@ -133,4 +133,22 @@ int main(){
     return 0;
 }
 //WAP  to check the no. is divisible by both 2 and 3 or divisible by only one if them
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
 
+    if(n%2==0 && n%3==0){
+        cout<<"Divisible by both"<<endl;
+    }
+    else if(n%2==0){
+        cout<<"Divisible by only 2"<<endl;
+    }
+    else if(n%3==0){
+        cout<<"Divisible by only 3"<<endl;
+    }
+    else{
+        cout<<"divisible by none"<<endl;
+    }
+}
