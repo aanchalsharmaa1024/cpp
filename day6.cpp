71007bc7d808add360e948b8bdbda140c137e4a5
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-06 17:39:03 +0530

    nested if

--- day6.cpp
+++ day6.cpp
@@ -15,3 +15,23 @@ int main(){
     }
     return 0;
 }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+
+    if(x==y){
+        cout<<"Both the numbers are equal";
+    }
+    else{
+        if(x>y){
+            cout<<"x is greater than y";
+        }
+        else{
+            cout<<"y is greater than y";
+        }
+    }
+    return 0;
+}
