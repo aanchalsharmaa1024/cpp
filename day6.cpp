ce3fc1844f1d60832bcb6a476793a194a70e20fb
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-06 20:42:57 +0530

    alphabet is vowel or not

--- day6.cpp
+++ day6.cpp
@@ -91,3 +91,25 @@ int main(){
     return 0;
 }
 //WAP to check if an alphabet is a vowel or a consonants
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    int isLowercaseVowel , isUppercaseVowel;
+    cout<<"Enter an alphabet:";
+    cin>>c;
+
+    isLowercaseVowel=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+ isUppercaseVowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+
+ if(isLowercaseVowel||isUppercaseVowel){
+     cout<<c<<"is a vowel";
+
+ }
+ else{
+     cout<<c<<"is a consonant";
+ }
+ return 0;
+
+    
+}
