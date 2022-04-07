1ec07d14d8ae0af90300a6b7e02e23fc75f5e8f9
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-08 01:20:15 +0530

    comment added

--- day7.cpp
+++ day7.cpp
@@ -108,4 +108,11 @@ int x = 2;
 (x & 1) ? printf("true") : printf("false");
 return 0;
 }
+//Ans. False
+//(As & is a unary operator we have to assume all decimal values to
+//binary(0's and 1's)
+//210 = 000000102
+//Now we go for condition (00000010 & 00000001)
+//Clearly, condition false as it leads to 0 when multiplied)
+
 
