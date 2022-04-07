bea6bdb808abce969160bb18dec08bfb2765335e
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-08 00:40:53 +0530

    code added

--- day7.cpp
+++ day7.cpp
@@ -1,1 +1,14 @@
 //WAP to display multiplication table upto 10
+[200~#include<iostream>
+using namespace std;
+int main(){
+		int n;
+			cout<<"enter a number:";
+				cin>>n;
+
+					for(int i=1; i<=10; ++i){
+								cout<<n<<"*"<<i<<"="<<n*i<<endl;
+									}
+						return 0;
+}
+
