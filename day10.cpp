72009d7871d1cf4e476078b28825f38fe5024ba9
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-08 01:56:55 +0530

    call by reference

--- day10.cpp
+++ day10.cpp
@@ -15,3 +15,28 @@ int main()
 
    return 0;
 }
+
+#include<iostream>
+using namespace std;
+
+void cyclicSwap(int *a, int *b, int *c);
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Enter value of a, b and c respectively: ";
+    cin >> a >> b >> c;
+
+    cout << "Value before swapping: " << endl;
+    cout << "a, b and c respectively are: " << a << ", " << b << ", " << c << endl;
+
+    cyclicSwap(&a, &b, &c);
+
+    cout << "Value after swapping numbers in cycle: " << endl;
+    cout << "a, b and c respectively are: " << a << ", " << b << ", " << c << endl;
+
+    return 0;
+}
+
+
