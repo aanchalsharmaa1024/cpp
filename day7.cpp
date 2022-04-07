1891b62076ecb49fa6fae9885dfe1fb146a16ece
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-04-08 00:49:11 +0530

    add +ve nos.

--- day7.cpp
+++ day7.cpp
@@ -12,4 +12,20 @@ int main(){
 						return 0;
 }
 //WAP to add only positive nos.
+#include<iostream>
+using namespace std;
+int main(){
+	int number;
+	int sum=0;
+	cout<<"enter a number:";
+	cin>>number;
 
+	while(number>=0){
+		sum+=number;
+		cout<<"enter a number:";
+		cin>>number;
+	}
+	cout<<"\n the sum is"<<sum<<endl;
+
+	return 0;
+}
