8a86b53470b1e8a898ca28998b980a61815f4ef7
Aarushi2021 <aarushi0015.be21@chitkara.edu.in>
2022-05-22 23:07:03 +0530

    merge

--- patterns.cpp
+++ patterns.cpp
@@ -6,12 +6,14 @@ int main()
 {
     int n;
     cin>>n;
-    for(int i=1; i<=n; i++){
+    
+    for(int i=1; i<=n; i++){//patterns
         for(int j=1; j<=i; j++){
             cout<<j;
         }
         cout<<endl;
-	cout<<"Hello world";
+	cout<<"Hello India";
+	//hello
     }
 
     return 0;
