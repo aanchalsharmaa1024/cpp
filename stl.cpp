9eed67cc06ee92b039ba9491df99827cc985df15
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-06-06 12:01:35 +0530

    deque

---
+++ stl.cpp
@@ -0,0 +1,47 @@
+//deque
+
+#include <iostream>
+#include<deque>
+using namespace std;
+
+int main()
+{
+    deque<int>d;
+    
+    d.push_back(1);
+    d.push_front(2);
+    
+    for(int i:d){
+        cout<<i<<" ";
+    }cout<<endl;
+    // d.pop_back();
+    // cout<<endl;
+    // for(int i:d){
+    //     cout<<i<<" ";
+    // }
+    
+    //  d.pop_front();
+    // cout<<endl;
+    // for(int i:d){
+    //     cout<<i<<" ";
+    // }
+    
+    cout<<d.at(1)<<endl;
+    cout<<d.front()<<endl;
+    cout<<d.back()<<endl;
+    cout<<d.empty()<<endl;
+    
+    cout<<d.size()<<endl;
+    
+    d.erase(d.begin(),d.begin()+2);
+    
+    cout<<d.size()<<endl;
+    
+    for(int i:d){
+        cout<<i<<endl;
+    }
+    
+
+    return 0;
+}
+
