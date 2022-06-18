1cb7e1a8d053409f66c05d28b53a7e6cba1096a6
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-06-18 19:14:14 +0530

    subarrays with equal 0's and 1's

--- arraypractice.cpp
+++ arraypractice.cpp
@@ -52,3 +52,50 @@ int main()
     }
 }
   // } Driver Code Ends
+  
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+  public:
+    //Function to count subarrays with 1s and 0s.
+    long long int countSubarrWithEqualZeroAndOne(int arr[], int n)
+    {
+        //Your code here
+
+        long long count=0, ans=0;
+        unordered_map<long long , long long>mp;
+        mp[0]=1;
+        for(int i=0; i<n; i++){
+            count += (arr[i]==1 ? 1:-1);
+            ans += mp[count];
+            mp[count]++;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n,i;
+	cin>>n;
+	int arr[n];
+	for(i=0;i<n;i++)
+	cin>>arr[i];
+	Solution obj;
+	cout<< obj.countSubarrWithEqualZeroAndOne(arr, n)<<"\n";
+	}
+	return 0;
+}
+  // } Driver Code Ends
