b24538e61ffb74f69e4a46484db44f028b91e8da
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-07-02 19:48:23 +0530

    aggressive cows

--- arraypractice.cpp
+++ arraypractice.cpp
@@ -572,3 +572,56 @@ int findPosition(vector<int>& arr, int n, int k)
 }
 
 
+
+
+
+
+//aggressive cows problem 
+
+
+bool isPossible(vector<int> &boards, int n, int k ,int mid){
+    int paintercount=1;
+    int boardsum=0;
+
+    for(int i=0; i<n; i++){
+        if(boardsum+ boards[i]<=mid){
+            boardsum += boards[i];
+        }
+
+        else{
+            paintercount++;
+            if(paintercount>k || boards[i]>mid){
+                return false;
+            }
+            boardsum =boards[i];
+        }
+    }
+    return true;
+}
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    //    Write your code here.
+    int s=0;
+    int sum=0;
+    int n= boards.size();
+    for(int i=0; i<n; i++){
+        sum+=boards[i];
+
+    }
+    int e=sum;
+    int ans=-1;
+    int mid=s+(e-s)/2;
+
+    while(s<=e){
+        if(isPossible(boards,n,k,mid)){
+            ans=mid;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
+
