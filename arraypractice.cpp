66fb9c71669acec1cdf2d4c299e0c4013e9c9a9c
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-08-01 12:48:08 +0530

    reverse a string

--- arraypractice.cpp
+++ arraypractice.cpp
@@ -625,3 +625,42 @@ int findLargestMinDistance(vector<int> &boards, int k)
     return ans;
 }
 
+
+
+
+
+//reverse a string
+#include <iostream>
+using namespace std;
+void reverse(char name[],int n){
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        swap(name[s],name[e]);
+        s++;
+        e--;
+    }
+}
+int getLength(char name[]){
+    int count=0;
+    for(int i=0; name[i]!='\0'; i++){
+        count++;
+    }
+    return count;
+}
+int main() {
+    char name[20];
+    cout<<"enter your name"<<endl;
+    cin>>name;
+    
+    cout<<"your name is ";
+    cout<<name<<endl;
+    int length= getLength(name);
+    
+cout<<"Length is:"<<length<<endl;
+reverse(name,length);
+cout<<"reversed name is ";
+cout<<name<<endl;
+    return 0;
+}
+
