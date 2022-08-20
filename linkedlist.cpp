84fd159088edb9cc672acbf0a229c5b9ea9e3834
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-08-20 21:26:49 +0530

    circular ll insertion, deletion

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -338,3 +338,108 @@ deleteNode(2,head);
 print(head);
     return 0;
 }
+
+
+
+
+
+
+//circular ll insertion and deletion
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+Node(int data){
+    this->data=data;
+    this->next=NULL;
+}    
+~Node(){
+    if(this->next!=NULL){
+        delete next;
+    }
+    next=NULL;
+}
+};
+void insertNode(Node* &tail, int element, int d){
+    if(tail==NULL){
+        Node* newNode = new Node(d);
+        tail=newNode;
+        //circular banao 
+        newNode->next=newNode;
+    }
+    else{
+        //non empty list
+        Node* curr =tail;
+        while(curr->data!=element){
+            curr=curr->next;
+        }
+        Node* temp=new Node(d);
+        temp->next=curr->next;
+        curr->next=temp;
+    }
+}
+void print(Node* &tail){
+    Node* temp=tail;
+    if(tail==NULL){
+        cout<<"list is empty";
+        return;
+    }
+    do{
+        cout<<tail->data<<" ";
+        tail=tail->next;
+    }
+    while(tail!=temp);
+    cout<<endl;
+    
+}
+
+void deleteNode(Node* &tail,int value){
+    //empty list
+    if(tail==NULL){
+     cout<<" list is empty,please check again"<<endl;
+     return;
+    }
+    else{
+        //non-empty
+        
+        //assuming that the value is present in the ll
+        Node* prev=tail;
+        Node* curr=prev->next;
+        
+        while(curr->data!=value){
+            prev=curr;
+            curr=curr->next;
+        }
+        prev->next=curr->next;
+        
+        //1 node ll
+        if(curr==prev){
+            tail=NULL;
+        }
+        //>=2 Node ll
+       else if(tail== curr){
+            tail=prev;
+        }
+        curr->next=NULL;
+        delete curr;
+    }
+}
+int main() {
+    Node* tail=NULL;
+insertNode(tail,2,12);
+print(tail);
+
+insertNode(tail,12,15);
+print(tail);
+
+insertNode(tail,15,67);
+print(tail);
+
+insertNode(tail,12,899);
+print(tail);
+deleteNode(tail,12);
+print(tail);
+    return 0;
+}
