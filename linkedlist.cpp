b626c5c629cf61ae6c5c3a3ac9a196e9a90f5298
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-08-21 19:13:51 +0530

    middle in ll

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -467,3 +467,29 @@ LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head)
     }
     return prev;
 }
+
+
+
+
+//middle in ll
+int getLength(Node* head){
+    int len=0;
+    while(head!=NULL){
+        len++;
+        head=head->next;
+    }
+    return len;
+}
+Node *findMiddle(Node *head) {
+    // Write your code here
+    int len=getLength(head);
+    int ans=(len/2);
+    Node* temp=head;
+    int cnt=0;
+    while(cnt<ans){
+        temp=temp->next;
+        cnt++;
+    }
+    return temp;
+    
+}
