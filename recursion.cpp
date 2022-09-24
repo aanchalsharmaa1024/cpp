749eab42a19aa6b0467efba918ec66b7869d7004
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-09-24 23:36:46 +0530

    stairs climb

--- recursion.cpp
+++ recursion.cpp
@@ -18,3 +18,23 @@ int n ;
  cout<<fib(n);
  
 }
+
+
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+int countDistinctWays(int nStairs) {
+    //  Write your code here.
+    if(nStairs<0)
+        return 0;
+
+    if(nStairs==0)
+        return 1;
+    int ans=countDistinctWays(nStairs-1)+countDistinctWays(nStairs-2);
+    return ans;
+}
